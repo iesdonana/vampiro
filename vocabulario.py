91834254e88d7e90eef148483d0fd9b7506f830b
Ricardo Pérez <ricpelo@gmail.com>
2021-02-22 20:37:30 +0100

    Nueva versión del vocabulario

--- vocabulario.py
+++ vocabulario.py
@@ -1,11 +1,27 @@
 class Token:
-    def __init__(self, ident):
-        self.__ident = ident
+    __tokens = {}
+
+    def __init__(self, lexemas):
+        self.__ident = lexemas[0]
+        for lexema in lexemas:
+            Token.__tokens[lexema] = self
 
     def __repr__(self):
         return self.__ident
 
+class Verbo(Token):
+    pass
+
+class Direccion(Verbo):
+    pass
+
+class Nombre(Token):
+    pass
+
+"""
 class Vocabulario:
+    palabras = {}
+
     def __init__(self, dic):
         self.__voc = {}
         for token, palabras in dic.items():
@@ -15,51 +31,35 @@ class Vocabulario:
     def token(self, palabra):
         return self.__voc.get(palabra)
 
+    @staticmethod
+    def es_direccion(palabra: Token):
+        return isinstance(palabra, Direccion)
+"""
 
-PALABRA_VERBO = 0
-PALABRA_NOMBRE = 1
+COGER = Verbo(['COGER', 'COGE', 'TOMAR', 'TOMA'])
+DEJAR = Verbo(['DEJAR', 'DEJA'])
+NORTE = Direccion(['NORTE', 'N'])
+SUR = Direccion(['SUR', 'S'])
+ESTE = Direccion(['ESTE', 'E'])
+OESTE = Direccion(['OESTE', 'O'])
+ARRIBA = Direccion(['ARRIBA', 'SUBIR'])
+ACABAR = Verbo(['ACABAR', 'TERMINAR', 'FIN'])
+TRAJE = Nombre(['TRAJE'])
+CUCHILLO = Nombre(['CUCHILLO'])
 
-TIPO_PALABRA = 0
-SIGNIF_PALABRA = 1
+"""
+verbos = Vocabulario({
+    COGER: ['COGER', 'COGE', 'TOMAR', 'TOMA'],
+    DEJAR: ['DEJAR', 'DEJA'],
+    NORTE: ['NORTE', 'N'],
+    SUR: ['SUR', 'S'],
+    ESTE: ['ESTE', 'E'],
+    OESTE: ['OESTE', 'O'],
+    ACABAR: ['ACABAR', 'FIN', 'TERMINAR'],
+})
 
-# Significados:
-COGER = 0
-DEJAR = 1
-TRAJE = 2
-CUCHILLO = 3
-
-palabras = [
-    ['COGER', PALABRA_VERBO, COGER],
-    ['COGE', PALABRA_VERBO, COGER],
-    ['DEJAR', PALABRA_VERBO, DEJAR],
-    ['TRAJE', PALABRA_NOMBRE, TRAJE],
-    ['CUCHILLO', PALABRA_NOMBRE, CUCHILLO],
-    ['NAVAJA', PALABRA_NOMBRE, CUCHILLO],
-    ['N', PALABRA_VERBO],
-    ['S', PALABRA_VERBO],
-    ['E', PALABRA_VERBO],
-    ['O', PALABRA_VERBO],
-    ['LLAVE', PALABRA_NOMBRE],
-    ['FIN', PALABRA_VERBO],
-    ['ACABAR', PALABRA_VERBO],
-]
-
-_coger = {
-    TIPO_PALABRA: PALABRA_VERBO,
-    SIGNIF_PALABRA: COGER,
-}
-
-_cuchillo = {
-    TIPO_PALABRA: PALABRA_NOMBRE,
-    SIGNIF_PALABRA: CUCHILLO,
-}
-
-palabras = {
-    'COGER': _coger,
-    'COGE': _coger,
-    'CUCHILLO': _cuchillo,
-    'NAVAJA': _cuchillo,
-}
-
-def es_direccion(verbo):
-    return verbo in ['N', 'S', 'E', 'O']
+nombres = Vocabulario({
+    TRAJE: ['TRAJE'],
+    CUCHILLO: ['CUCHILLO', 'NAVAJA'],
+})
+"""
